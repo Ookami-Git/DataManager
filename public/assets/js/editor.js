52eb52c0322abf5ae41e2bdd9982b6d2ab29d943
Ookami-Git <83712144+Ookami-Git@users.noreply.github.com>
2022-06-10 21:39:51 +0200

    [FIX] JSON parser

--- public/assets/js/editor.js
+++ public/assets/js/editor.js
@@ -458,7 +458,7 @@ function readPresentation(name) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            description=JSON.parse(JSON.stringify(this.response))
+            description=JSON.parse(this.response);
             description.presentation.forEach(function(presentation){
                 $(".btnAddPresentation:last").click();
                 $(".typeSelector:last").dropdown('set selected', presentation.type).change();
