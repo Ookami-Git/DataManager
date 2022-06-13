d1ba1d836d1862d797a7b6a4cbbf579970fee0b7
Ookami-Git <83712144+Ookami-Git@users.noreply.github.com>
2022-06-13 08:54:39 +0200

    Bug Fix

--- app/Views/templates/menu/m_footer.php
+++ app/Views/templates/menu/m_footer.php
@@ -22,17 +22,17 @@
             <button class="ui <?=themeClass?> primary mini button" onclick="$('.tableexport').table2csv({ filename: 'export.csv', separator: ';',  newline: '\n',  quoteFields: true,  excludeColumns: '.access_column',  excludeRows: ''});"><i class="download icon"></i> Export</button>
         </div>
 
-        <div class="ui <?=themeClass?>  dropdown icon item">
+        <div class="ui <?=themeClass?> dropdown icon item">
             <i class="bars icon"></i>
             <div class="menu">
                 <div class="header"><?= $user ?></div>
-                <a class="item" href="<?=base_url("/account")?>"><i class="user icon"></i>Mon compte</a>
-                <a class="item" href="<?=base_url("/logout")?>"><i class="sign out alternate icon"></i>Deconnexion</a>
+                <a class="dm item" href="<?=base_url("/account")?>"><i class="user icon"></i>Mon compte</a>
+                <a class="dm item" href="<?=base_url("/logout")?>"><i class="sign out alternate icon"></i>Deconnexion</a>
                 <?php if (in_array("admin",session()->get("roles")) == true || session()->get("username") == "admin"): ?>
                 <div class="divider"></div>
                 <div class="header">Administration</div>
-                <a class="item" href="<?=base_url("/admin/general")?>"><i class="tools icon"></i>Paramètres</a>
-                <a class="item" href="<?=base_url("/editor")?>"><i class="layer group icon"></i>Modifier pages</a>
+                <a class="dm item" href="<?=base_url("/admin/general")?>"><i class="tools icon"></i>Paramètres</a>
+                <a class="dm item" href="<?=base_url("/editor")?>"><i class="layer group icon"></i>Modifier pages</a>
                 <?php endif ?>
             </div>
         </div>
